e3ba3b7b31aa66263abfcf81fc19ea329e942fa9
goberomsu <gobeumsu@gmail.com>
2023-08-04 20:19:03 +0900

    Fix : excluded bullet of description
    sorry. this is typo

--- src/main.ts
+++ src/main.ts
@@ -91,7 +91,7 @@ export default class ICSPlugin extends Plugin {
                         `- [ ] ${e.time}`,
                         e.format.icsName ? e.icsName : null,
                         e.format.summary ? e.summary : null,
-                        e.format.description ? `\n\t${e.description}` : null,
+                        e.format.description ? `\n\t- ${e.description}` : null,
                     ].filter(Boolean).join(' ')
 				});			
 				editor.replaceRange(mdArray.sort().join("\n"), editor.getCursor());
