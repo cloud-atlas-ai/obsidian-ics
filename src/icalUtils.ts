fa96d135cda442db18ff0fbd44aa2453c3b53265
Muness Castle <931+muness@users.noreply.github.com>
2023-10-25 10:56:56 -0400

    simplify rrule parsing.
    Still have some missing events and maybe some TZ issues.

--- src/icalUtils.ts
+++ src/icalUtils.ts
@@ -1,6 +1,7 @@
 const ical = require('node-ical');
 import { tz } from 'moment-timezone';
 import { moment } from "obsidian";
+import { RRule, RRuleSet, rrulestr } from 'rrule';
 
 export function extractMeetingInfo(e: any): { callUrl: string, callType: string } {
 
@@ -27,90 +28,44 @@ export function extractMeetingInfo(e: any): { callUrl: string, callType: string 
 }
 
 export function filterMatchingEvents(icsArray: any[], dayToMatch: string) {
-	var matchingEvents = [];
-	matchingEvents = findRecurringEvents(icsArray, dayToMatch);
+	const localTimezone = tz.guess();
 
-	// find non-recurring events on the day
-	icsArray.map((e) => {
-		if (moment(e.start).isSame(dayToMatch, "day")) {
-			matchingEvents.push(e);
-		}
-	});
-
-	return matchingEvents;
-}
-
-function findRecurringEvents(icsArray: any[], dayToMatch: string) {
-	var matchingRecurringEvents: any[] = [];
-
-	const rangeStart = moment(dayToMatch);
-	const rangeEnd = moment(dayToMatch).add(1439, 'minutes');
-	icsArray.forEach(origEvent => {
-
-		// Add recurrences moved to dayToMatch
-		if (origEvent.recurrences !== undefined) {
-			for (let date in origEvent.recurrences) {
-				const recurrence = origEvent.recurrences[date];
-				if (recurrence.start.toISOString().slice(0, 10) === rangeStart.toISOString().slice(0, 10)) {
-					matchingRecurringEvents.push(recurrence);
+	return icsArray.reduce((matchingEvents, event) => {
+		if (event.recurrences !== undefined) {
+			for (let date in event.recurrences) {
+				const recurrence = event.recurrences[date];
+				if (moment(recurrence.start).isSame(dayToMatch, "day")) {
+					matchingEvents.push(recurrence);
 				}
 			}
 		}
+		if (typeof event.rrule !== 'undefined') {
+			console.log(event);
+			event.rrule.between(moment(dayToMatch).startOf('day').toDate(), moment(dayToMatch).startOf('day').toDate()).forEach(date => {
+				// We need to clone the existing event and override the start and end date.
+				const clonedEvent = { ...event };
+				const localDate = tz(date, moment.tz.guess());
+				clonedEvent.start = localDate.toDate();
 
-		if (typeof origEvent.rrule !== 'undefined') {
-			const duration = extractDuration(origEvent);
+				if (event.duration) {
+					clonedEvent.end = localDate.clone().add(event.duration, 'milliseconds');
+				}
 
-			// Complicated case - if an RRULE exists, handle multiple recurrences of the event.
-			// For recurring events, get the set of event start dates that fall within the range
-			// of dates we're looking for.
-			var dates = origEvent.rrule.between(rangeStart.toDate(), rangeEnd.toDate(), true, () => {
-				return true;
+				// Remove rrule property from clonedEvent
+				delete clonedEvent.rrule;
+
+				matchingEvents.push(clonedEvent);
 			});
-
-			// Loop through the set of date entries to see which recurrences should be included.
-			dates.forEach(date => {
-				let curDuration = duration;
-				let curEvent = origEvent;
-				let skip = false;
-
-				// Use just the date of the recurrence to look up overrides and exceptions (i.e. chop off time information)
-				const dateLookupKey = date.toISOString().slice(0, 10);
-
-				if (origEvent.exdate !== undefined && curEvent.exdate[dateLookupKey] !== undefined) {
-					// If there's no recurrence override, check for an exception date.  Exception dates represent exceptions to the rule.
-					// This date is an exception date, which means we should skip it in the recurrence pattern.
-					skip = true;
-				}
-
-				// For each date that we're checking, it's possible that there is a recurrence override for that one day.
-				if (curEvent.recurrences !== undefined && curEvent.recurrences[dateLookupKey] !== undefined) {
-					// override event
-					curEvent = curEvent.recurrences[dateLookupKey];
-					//override duration
-					curDuration = extractDuration(curEvent);
-				}
-
-				//if this is the first instance of the event, we don't want it picked up here
-				if (moment(date).isSame(curEvent.start)) {
-					skip = true;
-				}
-
-				if (!skip)
-					matchingRecurringEvents.push(cloneRecurringEvent(origEvent, curEvent, date, curDuration));
-			});
+		} else {
+			if (moment(event.start).isSame(dayToMatch, "day")) {
+				matchingEvents.push(event);
+			}
 		}
-	});
-
-	return matchingRecurringEvents;
-
+		return matchingEvents;
+	}, []);;
 }
 
-function extractDuration(event: any) {
-	return (Number.parseInt(moment(event.end).format('x'), 10) - Number.parseInt(moment(event.start).format('x'), 10));
-
-}
-
-function applyTzOffset(origEvent: any, event: any, date: any) {
+function applyTzOffset(origEvent: any, start: any, date: any) {
 	if (origEvent.rrule != undefined && origEvent.rrule.origOptions.tzid) {
 		// tzid present on the rrule
 		const eventTimeZone = tz.zone(origEvent.rrule.origOptions.tzid);
@@ -119,7 +74,7 @@ function applyTzOffset(origEvent: any, event: any, date: any) {
 		return moment(date).add(offset, 'minutes');
 	} else {
 		// tzid not present on rrule (calculate offset from original start)
-		return moment(new Date(date.setHours(date.getHours() - ((event.start.getTimezoneOffset() - date.getTimezoneOffset()) / 60))));
+		return moment(new Date(date.setHours(date.getHours() - ((start.getTimezoneOffset() - date.getTimezoneOffset()) / 60))));
 	}
 }
 
