53879088de236a5dd512c3405dc687df90c8b074
Ben Pannier <bpannier@users.noreply.github.com>
2024-11-18 12:43:43 +0100

    Update IEvent.ts
    Adding known parameter: created, sequence, lastModified, organizer interface

--- src/IEvent.ts
+++ src/IEvent.ts
@@ -4,6 +4,9 @@ export interface IEvent {
 	utime: string; // Unix timestamp representing the event start time
 	time: string; // Human-readable representation of the event start time
 	endTime: string; // Human-readable representation of the event end time
+	created: string; // Human-readable representation of the creation timestamp of the event
+	sequence: number; // The revision sequence number of the calendar component within a sequence of revisions.
+	lastModified: string; // Human-readable representation of when the event was last revised
 	icsName: string; // Name of the calendar the event is associated with
 	summary: string; // Summary or title of the event
 	description: string; // Detailed description of the event
@@ -11,6 +14,7 @@ export interface IEvent {
 	location: string; // Physical location where the event takes place, if applicable
 	callUrl: string; // URL for joining online meetings/calls associated with the event
 	callType: string; // Type of online meeting (e.g., Zoom, Skype, etc.)
+	organizer: IOrganizer; // Email of the organizer of the event
   attendees: IAttendee[]; // Array of attendees
 }
 
@@ -19,4 +23,10 @@ export interface IAttendee {
   name: string;
   role: string;
   status: string; // Participation status (accepted, declined, etc.)
+  type: string; // Participant type (individual, group, resource, room, etc.)
+}
+
+export interface IOrganizer {
+  email: string;
+  name: string;
 }
