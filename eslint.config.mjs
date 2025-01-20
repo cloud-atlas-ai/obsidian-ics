3c02d1708d32cff46d3d512da62271e7a8a072cf
Muness Castle <931+muness@users.noreply.github.com>
2025-01-19 20:15:18 -0500

    refactor: update ESLint configuration and dependencies for improved TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
-import config from "eslint-config-standard";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  ...[].concat(config),
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
